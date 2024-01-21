06d396e4f2de33f8cc2e24edee2615156a109efe
Angel Hernandez <angel.hdz.ojd@gmail.com>
2024-01-21 13:49:51 +0000

    Third commit, with the start of the second operator (pow)

--- src/main/java/kata6/Math.java
+++ src/main/java/kata6/Math.java
@@ -3,17 +3,33 @@ package kata6;
 public class Math {
     private String input1;
     private String operator;
+    private String input2;
 
-    public Math(String input1, String operator) {
+    public Math(String input1, String operator, String input2) {
         this.input1 = input1;
         this.operator = operator;
+        this.input2 = input2;
     }
 
     public String execute(){
+        switch (operator){
+            case "!":
+                return factorial();
+            case "^":
+                return pow();
+        }
+        return "No se encuentra el operador";
+    }
+
+    public String factorial(){
         int result=1;
         for(int i=1; i<=Integer.parseInt(input1); i++){
             result *= i;
         }
         return Integer.toString(result);
+    }
+
+    public String pow(){
+        return "";
     }
 }
--- src/test/java/MathTest.java
+++ src/test/java/MathTest.java
@@ -5,16 +5,21 @@ import static org.assertj.core.api.Assertions.*;
 public class MathTest {
     @Test
     public void should_return_120_given_5_factorial(){
-        assertThat(new Math("5", "!").execute()).isEqualTo("120");
+        assertThat(new Math("5", "!", "").execute()).isEqualTo("120");
     }
 
     @Test
     public void should_return_5040_given_7_factorial(){
-        assertThat(new Math("7", "!").execute()).isEqualTo("5040");
+        assertThat(new Math("7", "!", "").execute()).isEqualTo("5040");
     }
 
     @Test
     public void should_return_362880_given_9_factorial(){
-        assertThat(new Math("9", "!").execute()).isEqualTo("362880");
+        assertThat(new Math("9", "!", "").execute()).isEqualTo("362880");
+    }
+
+    @Test
+    public void should_return_4_given_2_pow_2(){
+        assertThat(new Math("2", "^", "2").execute()).isEqualTo("4");
     }
 }
